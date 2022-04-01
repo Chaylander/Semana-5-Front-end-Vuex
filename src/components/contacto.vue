9d011dbfb3068f70fa538b0761267de655c48ad7
RobertoJáuregui <robotrjj@hotmail.com>
2022-04-01 12:14:06 -0600

    Iconos redes por Copilot

--- src/components/contacto.vue
+++ src/components/contacto.vue
@@ -3,6 +3,17 @@
     Encuentranos en:Lorem ipsum dolor sit. Telefono:xxxxxxxxxxxx Abierto de
     Lunes a viernes de 9:00am a 8:00pm
   </p>
+  <p>
+    <a href="https://www.facebook.com/">
+      <img src="https://img.icons8.com/color/48/000000/facebook-new.png" />
+    </a>
+    <a href="https://www.instagram.com/">
+      <img src="https://img.icons8.com/color/48/000000/instagram-new.png" />
+    </a>
+    <a href="https://www.twitter.com/">
+      <img src="https://img.icons8.com/color/48/000000/twitter.png" />
+    </a>
+    </p>
 </template>
 
 <script>
